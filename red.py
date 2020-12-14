8fef5922c2c79a9aeb63dab430bf5952fccfac20
bitglass-dev <65251565+bitglass-dev@users.noreply.github.com>
2020-12-14 14:15:11 -0800

    Create red.py

---
+++ red.py
@@ -0,0 +1,8 @@
+https://portal.us.bgd4.net/admin/apiSetup/?app=githubhttps://portal.us.bgd4.net/admin/apiSetup/?app=github
+https://portal.us.bgd4.net/admin/apiSetup/?app=github
+lana
+red
+yellow
+black
+blue
+red
